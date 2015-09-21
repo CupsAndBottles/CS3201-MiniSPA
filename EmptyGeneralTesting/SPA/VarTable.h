aad8af567bc74da5d4b98e53cb862910b5e1aaa4
gangadevi <gangadevi.balakrishnan@gmail.com>
2015-09-21 17:26:44 +0800

    Table files for PKB uploaded.

--- EmptyGeneralTesting/SPA/VarTable.h
+++ EmptyGeneralTesting/SPA/VarTable.h
@@ -1,8 +1,21 @@
 #pragma once
+#include "Variable.h"
+
 class VarTable
 {
+private:
+	vector<Variable> varTable;
 public:
 	VarTable();
 	~VarTable();
+	int insertVar(int index, string VarName, int procNames, int usedBy, int modifiedBy);
+	int getIndex(string varName);
+	int getNoOfVar();
+	vector<string> getAllVar();
+	string getVarName(int index);
+	vector<int> getStmtNumUsingVar(string var);
+	vector<int> getStmtForModified(string var);
+	vector<int> getProcNames(string var);
+	bool isVariableName(string varName);
 };
 
