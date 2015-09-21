bf04a9d7badfc5eef7b3cf7a56cb89589de04ed0
gangadevi <gangadevi.balakrishnan@gmail.com>
2015-09-21 23:40:12 +0800

    New Files uploaded. UnitTesting works now.

---
+++ EmptyGeneralTesting/UnitTesting/TestParser.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "Parser.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTesting
+{
+	TEST_CLASS(TestParser)
+	{
+	public:
+
+		TEST_METHOD(TestParentMethod)
+		{
+		//	Parser T;
+			// TODO: Your test code here
+		}
+
+	};
+}
