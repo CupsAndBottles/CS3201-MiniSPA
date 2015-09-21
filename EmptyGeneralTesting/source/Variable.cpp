dfe6146dd6bf731db4b781762f36a504fb09ee75
gangadevi <gangadevi.balakrishnan@gmail.com>
2015-09-22 00:10:43 +0800

    VarTable for PKB

--- EmptyGeneralTesting/source/Variable.cpp
+++ EmptyGeneralTesting/source/Variable.cpp
@@ -1,1 +1,91 @@
-#pragma once
+#include "Variable.h"
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+Variable::Variable()
+{
+}
+
+Variable::Variable(string VarName, int procNames, int usedBy, int modifiedBy) {
+	setVarName(VarName);
+	setProcNames(procNames);
+	setUsedBy(usedBy);
+	setModifiedBy(modifiedBy);
+}
+
+
+Variable::~Variable()
+{
+}
+
+
+void Variable::setVarName(string varName) {
+	this->varName = varName;
+}
+
+void Variable::setProcNames(int proc) {
+	procNamesList.push_back(proc);
+}
+
+void Variable::setUsedBy(int stmtLst) {
+	if (stmtLst != 0) {
+		usedByList.push_back(stmtLst);
+	}
+}
+
+void Variable::setModifiedBy(int stmtLst) {
+	if (stmtLst != 0) {
+		modifiedByList.push_back(stmtLst);
+	}
+}
+void Variable::sortVectors(vector<int> list) {
+	sort(list.begin(), list.end());
+}
+
+int Variable::getindex() {
+	return this->index;
+}
+
+string Variable::getVarName() {
+	return this->varName;
+}
+
+vector<int> Variable::setProcNames() {
+	return this->procNamesList;
+}
+
+vector<int> Variable::getUsedBy() {
+	return this->usedByList;
+}
+
+vector<int> Variable::getModifiedBy() {
+	return this->modifiedByList;
+}
+
+vector<int> Variable::getProcNames() {
+	return this->procNamesList;
+}
+void Variable::insertIntoModify(int modify) {
+	sortVectors(modifiedByList);
+	if (modify != 0 && !binary_search(modifiedByList.begin(), modifiedByList.end(), modify)) {
+		modifiedByList.push_back(modify);
+	}
+}
+
+void Variable::insertIntoUses(int use) {
+	sortVectors(usedByList);
+	if (use != 0 && !binary_search(usedByList.begin(), usedByList.end(), use)) {
+		usedByList.push_back(use);
+	}
+}
+
+void Variable::insertIntoProc(int procName) {
+	sortVectors(procNamesList);
+	if (procName != 0 && !binary_search(procNamesList.begin(), procNamesList.end(), procName)) {
+		procNamesList.push_back(procName);
+	}
+}
