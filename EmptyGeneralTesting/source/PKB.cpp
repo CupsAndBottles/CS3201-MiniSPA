469f8bf95c168843e2afd5324da782aca35b8f94
sharonmat.194 <sharonmat.194@gmail.com>
2015-09-22 20:00:57 +0800

    Adding methods to PKB class

--- EmptyGeneralTesting/source/PKB.cpp
+++ EmptyGeneralTesting/source/PKB.cpp
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <stddef.h> //defines null
 #include<stdio.h>
 #include <iostream>
 #include <string>
@@ -8,12 +8,124 @@
 using namespace std;
 
 #include "PKB.h"
-#include "TNode.h"
+
+PKB* PKB::m_Instance = NULL;
+
+PKB* PKB::getInstanceOf()
+{
+	if (m_Instance)
+		m_Instance = new PKB;
+	return m_Instance;
+}
+
+
+//PKB::PKB()
+//{
+//}
+
+PKB::~PKB()
+{
+}
 
 int PKB::setProcToAST(PROC p, TNode* r) {
 	return NULL;
 }
 
-TNode* PKB::getRootAST (PROC p){
+TNode* PKB::getRootAST(PROC p) {
 	return NULL;
 }
+
+std::vector<pair<int, int>> PKB::getCalls(TYPE, int)
+{
+	return std::vector<pair<int, int>>();
+}
+std::vector<pair<int, int>> PKB::getUses(TYPE, int)
+{
+	return std::vector<pair<int, int>>();
+}
+std::vector<pair<int, int>> PKB::getParent(TYPE, int)
+{
+	return std::vector<pair<int, int>>();
+}
+
+std::vector<pair<int, int>> PKB::getFollows(TYPE, int)
+{
+	return std::vector<pair<int, int>>();
+}
+
+std::vector<pair<int, int>> PKB::getParentT(TYPE, int)
+{
+	return std::vector<pair<int, int>>();
+}
+
+std::vector<pair<int, int>> PKB::getFollowsT(TYPE, int)
+{
+	return std::vector<pair<int, int>>();
+}
+
+
+void PKB::getProcNameInVarTable(int, string)
+{
+}
+
+void PKB::getUsedByStmtNum(int, int)
+{
+}
+
+void PKB::getModifiedByStmtNum(int, int)
+{
+}
+
+void PKB::extractParent(int)
+{
+
+}
+
+void PKB::extractChildren(int)
+{
+}
+
+void PKB::extractFollowsT(int)
+{
+}
+
+void PKB::extractFollowedByT(int)
+{
+}
+
+
+int PKB::getProcIndex(string)
+{
+	return 0;
+}
+
+string PKB::getProcName(int)
+{
+	return string();
+}
+
+int PKB::getVarIndex(string varName)
+{
+
+	return 0;
+}
+
+string PKB::getVarName(int index)
+{
+
+	return 0;
+
+}
+
+int PKB::getWholeStmt(int, int)
+{
+	return 0;
+}
+
+void PKB::getIfStmt(int, int)
+{
+}
+
+
+
+
