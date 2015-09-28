ea113913f3907ef38b79429b2c90a98100761272
Weinilin <weinilinhappy@gmail.com>
2015-09-28 17:35:05 +0800

    changes to preprocessor pql

---
+++ EmptyGeneralTesting/source/ParserOfType.h
@@ -0,0 +1,25 @@
+#pragma once
+#pragma once
+#include<vector>
+#include<string>
+#include<regex>
+
+using namespace std;
+class ParserOfType
+{
+public:
+	ParserOfType();
+	~ParserOfType();
+
+	vector<vector<string>> setType(int clauseType, string synonym, vector<string> type, vector<string> synonymType);
+	vector<vector<string>> setClauseType(int clauseType, string synonym, vector<string> type, vector<string> synonymType);
+	bool isSynDigit(string syn);
+	int isBeingDeclared(string syn, vector<string> synType);
+	string checkSubExpression(string arg2);
+	string removeUnwanted(string syn);
+	bool isVariable(string syn);
+	bool isProcedure(string syn);
+	bool isExpression(string syn);
+};
+
+
