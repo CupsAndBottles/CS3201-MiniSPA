9b8fb0caf40a9d47c127af5ce5311e3c74c5cd2d
Zhi Hua <ngzhihua_@hotmail.com>
2015-10-03 15:10:00 +0800

    Added TestQueryEvaluator.cpp

---
+++ EmptyGeneralTesting/UnitTesting/TestQueryEvaluator.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "QueryEvaluator.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTesting
+{
+	TEST_CLASS(TestQueryEvaluator)
+	{
+	public:
+		TEST_METHOD(TestEvaluateQuery){
+
+
+		}
+	};
+}
