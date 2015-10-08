dd49a44d5f50f2be9a40373cae089fcfe6507650
XianHui <xianhui.koh@gmail.com>
2015-10-09 00:10:07 +0800

    remove setting of T methods

--- EmptyGeneralTesting/UnitTesting/TestQueryEvaluator.cpp
+++ EmptyGeneralTesting/UnitTesting/TestQueryEvaluator.cpp
@@ -31,17 +31,19 @@ namespace UnitTesting
 		} 
 
 		TEST_METHOD(QE_EvaluateSuchThatFollowsGiven) {
-			/*********************** Test Code ************************/
-			/*	beads = command + 10;							\\1
-			beads = 34;										\\2
-			command = inspiration + 1;						\\3
-			while coffee {									\\4
-			inspiration = beads + command + coffee;		\\5
-			while command {								\\6
-			x = x + 9;}								\\7
-			x = beads + command; }						\\8
-			*/
-			/**********************************************************/
+		/*********************** Test Code ************************/
+		/*	
+			procedure dream {
+				beads = command + 10;							\\1
+				beads = 34;										\\2
+				command = inspiration + 1;						\\3
+				while coffee {									\\4
+					inspiration = beads + command + coffee;		\\5
+					while command {								\\6
+						x = x + 9;}								\\7
+					x = beads + command; }						\\8
+		*/
+		/**********************************************************/
 			PKB *pkb = new PKB();
 
 			pkb->setType(Enum::TYPE::ASSIGN);	//1
@@ -62,13 +64,14 @@ namespace UnitTesting
 
 			pkb->setProcNameInProcTable("dream");	//0
 
-													// Set ALL follows
+			// Set ALL follows
 			vector<pair<int, int>> follows;
 			follows.push_back(make_pair(1, 2));
 			follows.push_back(make_pair(2, 3));
 			follows.push_back(make_pair(3, 4));
 			follows.push_back(make_pair(5, 6));
-			follows.push_back(make_pair(7, 8));
+			follows.push_back(make_pair(5, 8));
+			follows.push_back(make_pair(6, 8));
 			pkb->setFollows(follows);
 
 			// Set ALL Parent
@@ -78,21 +81,6 @@ namespace UnitTesting
 			parent.push_back(make_pair(4, 8));
 			parent.push_back(make_pair(6, 7));
 			pkb->setChildren(parent);
-
-			vector<int> childrenT = { 5, 6, 7, 8 };
-			pkb->setChildrenT(4, childrenT);
-
-			childrenT.clear();
-			childrenT = { 7 };
-			pkb->setParentT(6, childrenT);
-
-			vector<int> parentT = { 4 };
-			pkb->setParentT(5, parentT);
-			pkb->setParentT(6, parentT);
-			pkb->setParentT(8, parentT);
-
-			parentT.push_back(6);
-			pkb->setParentT(7, parentT);
 
 			// Statement 1 - set constant as variables?
 			pkb->setVarName("beads");
@@ -185,7 +173,9 @@ namespace UnitTesting
 
 		TEST_METHOD(QE_EvaluateSuchThatParentUnderscore) {
 		/*********************** Test Code ************************/
-			/*	beads = command + 10;							\\1
+		/*	
+			procedure dream {
+				beads = command + 10;							\\1
 				beads = 34;										\\2
 				command = inspiration + 1;						\\3
 				while coffee {									\\4
@@ -193,7 +183,7 @@ namespace UnitTesting
 					while command {								\\6	
 						x = x + 9;}								\\7	
 					x = beads + command; }						\\8
-			*/
+		*/
 		/**********************************************************/
 			PKB *pkb = new PKB();
 
@@ -221,7 +211,8 @@ namespace UnitTesting
 			follows.push_back(make_pair(2, 3));
 			follows.push_back(make_pair(3, 4));
 			follows.push_back(make_pair(5, 6));
-			follows.push_back(make_pair(7, 8));
+			follows.push_back(make_pair(5, 8));
+			follows.push_back(make_pair(6, 8));
 			pkb->setFollows(follows);
 
 			// Set ALL Parent
@@ -231,20 +222,6 @@ namespace UnitTesting
 			parent.push_back(make_pair(4, 8));
 			parent.push_back(make_pair(6, 7));
 			pkb->setChildren(parent);
-
-			vector<int> childrenT = { 5, 6, 7, 8 };
-			pkb->setChildrenT(4, childrenT);
-
-			childrenT = { 7 };
-			pkb->setParentT(6, childrenT);
-			
-			vector<int> parentT = { 4 };
-			pkb->setParentT(5, parentT);
-			pkb->setParentT(6, parentT);
-			pkb->setParentT(8, parentT);
-
-			parentT = { 4, 6 };
-			pkb->setParentT(7, parentT);
 
 			// Statement 1 - set constant as variables?
 			pkb->setVarName("beads");
@@ -376,7 +353,8 @@ namespace UnitTesting
 			follows.push_back(make_pair(2, 3));
 			follows.push_back(make_pair(3, 4));
 			follows.push_back(make_pair(5, 6));
-			follows.push_back(make_pair(7, 8));
+			follows.push_back(make_pair(5, 8));
+			follows.push_back(make_pair(6, 8));
 			pkb->setFollows(follows);
 
 			// Set ALL Parent
@@ -386,21 +364,6 @@ namespace UnitTesting
 			parent.push_back(make_pair(4, 8));
 			parent.push_back(make_pair(6, 7));
 			pkb->setChildren(parent);
-
-			vector<int> childrenT = { 5, 6, 7, 8 };
-			pkb->setChildrenT(4, childrenT);
-
-			childrenT.clear();
-			childrenT = { 7 };
-			pkb->setParentT(6, childrenT);
-
-			vector<int> parentT = { 4 };
-			pkb->setParentT(5, parentT);
-			pkb->setParentT(6, parentT);
-			pkb->setParentT(8, parentT);
-
-			parentT.push_back(6);
-			pkb->setParentT(7, parentT);
 
 			// Statement 1 - set constant as variables?
 			pkb->setVarName("beads");
@@ -496,15 +459,15 @@ namespace UnitTesting
 		TEST_METHOD(QE_PatternSubExpression) {
 			/*********************** Test Code ************************/
 			/*
-			Procedure dream {
-				beads = command + 10;							\\1
-				beads = 34;										\\2
-				command = inspiration + 1;						\\3
-				while coffee {									\\4
-					stamps = beads + command + coffee;			\\5
-					while command {								\\6
-						x = x + x * 9;}							\\7
-						x = beads + command; }}					\\8
+				procedure dream {
+					beads = command + 10;							\\1
+					beads = 34;										\\2
+					command = inspiration + 1;						\\3
+					while coffee {									\\4
+						stamps = beads + command + coffee;			\\5
+						while command {								\\6
+							x = x + x * 9;}							\\7
+						x = beads + command; }}						\\8
 			*/
 			/**********************************************************/
 			PKB *pkb = new PKB();
@@ -533,7 +496,8 @@ namespace UnitTesting
 			follows.push_back(make_pair(2, 3));
 			follows.push_back(make_pair(3, 4));
 			follows.push_back(make_pair(5, 6));
-			follows.push_back(make_pair(7, 8));
+			follows.push_back(make_pair(5, 8));
+			follows.push_back(make_pair(6, 8));
 			pkb->setFollows(follows);
 
 			// Set ALL Parent
@@ -543,21 +507,6 @@ namespace UnitTesting
 			parent.push_back(make_pair(4, 8));
 			parent.push_back(make_pair(6, 7));
 			pkb->setChildren(parent);
-
-			vector<int> childrenT = { 5, 6, 7, 8 };
-			pkb->setChildrenT(4, childrenT);
-
-			childrenT.clear();
-			childrenT = { 7 };
-			pkb->setParentT(6, childrenT);
-
-			vector<int> parentT = { 4 };
-			pkb->setParentT(5, parentT);
-			pkb->setParentT(6, parentT);
-			pkb->setParentT(8, parentT);
-
-			parentT.push_back(6);
-			pkb->setParentT(7, parentT);
 
 			// Statement 1 - set constant as variables?
 			pkb->setVarName("beads");
@@ -659,15 +608,15 @@ namespace UnitTesting
 		TEST_METHOD(QE_PatternRightDefined) {
 			/*********************** Test Code ************************/
 			/*
-			Procedure dream {
-			beads = command + 10;							\\1
-			beads = 34;										\\2
-			command = inspiration + 1;						\\3
-			while coffee {									\\4
-			stamps = beads + command + coffee;			\\5
-			while command {								\\6
-			x = x + x * 9;}							\\7
-			x = beads + command; }}					\\8
+				procedure dream {
+					beads = command + 10;							\\1
+					beads = 34;										\\2
+					command = inspiration + 1;						\\3
+					while coffee {									\\4
+						stamps = beads + command + coffee;			\\5
+						while command {								\\6
+							x = x + x * 9;}							\\7
+						x = beads + command; }}						\\8
 			*/
 			/**********************************************************/
 			PKB *pkb = new PKB();
@@ -690,19 +639,21 @@ namespace UnitTesting
 
 			pkb->setProcNameInProcTable("dream");	//0
 
-													// Set ALL follows
+			// Set ALL follows
 			vector<pair<int, int>> follows;
 			follows.push_back(make_pair(1, 2));
 			follows.push_back(make_pair(2, 3));
 			follows.push_back(make_pair(3, 4));
 			follows.push_back(make_pair(5, 6));
-			follows.push_back(make_pair(7, 8));
+			follows.push_back(make_pair(5, 8));
+			follows.push_back(make_pair(6, 8));
 			pkb->setFollows(follows);
 
 			// Set ALL Parent
 			vector<pair<int, int>> parent;
 			parent.push_back(make_pair(4, 5));
 			parent.push_back(make_pair(4, 6));
+			parent.push_back(make_pair(4, 8));
 			parent.push_back(make_pair(6, 7));
 			pkb->setChildren(parent);
 
@@ -805,15 +756,15 @@ namespace UnitTesting
 		TEST_METHOD(QE_PatternNonSubExpression) {
 			/*********************** Test Code ************************/
 			/*
-			Procedure dream {
-			beads = command + 10;							\\1
-			beads = 34;										\\2
-			command = inspiration + 1;						\\3
-			while coffee {									\\4
-			stamps = beads + command + coffee;			\\5
-			while command {								\\6
-			x = x + x * 9;}							\\7
-			x = beads + command; }}					\\8
+				procedure dream {
+					beads = command + 10;							\\1
+					beads = 34;										\\2
+					command = inspiration + 1;						\\3
+					while coffee {									\\4
+						stamps = beads + command + coffee;			\\5
+						while command {								\\6
+							x = x + x * 9;}							\\7
+						x = beads + command; }}						\\8
 			*/
 			/**********************************************************/
 			PKB *pkb = new PKB();
@@ -836,19 +787,21 @@ namespace UnitTesting
 
 			pkb->setProcNameInProcTable("dream");	//0
 
-													// Set ALL follows
+			// Set ALL follows
 			vector<pair<int, int>> follows;
 			follows.push_back(make_pair(1, 2));
 			follows.push_back(make_pair(2, 3));
 			follows.push_back(make_pair(3, 4));
 			follows.push_back(make_pair(5, 6));
-			follows.push_back(make_pair(7, 8));
+			follows.push_back(make_pair(5, 8));
+			follows.push_back(make_pair(6, 8));
 			pkb->setFollows(follows);
 
 			// Set ALL Parent
 			vector<pair<int, int>> parent;
 			parent.push_back(make_pair(4, 5));
 			parent.push_back(make_pair(4, 6));
+			parent.push_back(make_pair(4, 8));
 			parent.push_back(make_pair(6, 7));
 			pkb->setChildren(parent);
 
@@ -951,15 +904,15 @@ namespace UnitTesting
 		TEST_METHOD(QE_SuchThatPlusPattern) {
 			/*********************** Test Code ************************/
 			/*
-			Procedure dream {
-			beads = command + 10;							\\1
-			beads = 34;										\\2
-			command = inspiration + 1;						\\3
-			while coffee {									\\4
-			stamps = beads + command + coffee;			\\5
-			while command {								\\6
-			x = x + x * 9;}							\\7
-			x = beads + command; }}					\\8
+				procedure dream {
+					beads = command + 10;							\\1
+					beads = 34;										\\2
+					command = inspiration + 1;						\\3
+					while coffee {									\\4
+						stamps = beads + command + coffee;			\\5
+						while command {								\\6
+							x = x + x * 9;}							\\7
+						x = beads + command; }}						\\8
 			*/
 			/**********************************************************/
 			PKB *pkb = new PKB();
@@ -988,7 +941,8 @@ namespace UnitTesting
 			follows.push_back(make_pair(2, 3));
 			follows.push_back(make_pair(3, 4));
 			follows.push_back(make_pair(5, 6));
-			follows.push_back(make_pair(7, 8));
+			follows.push_back(make_pair(5, 8));
+			follows.push_back(make_pair(6, 8));
 			pkb->setFollows(follows);
 
 			// Set ALL Parent
@@ -998,21 +952,6 @@ namespace UnitTesting
 			parent.push_back(make_pair(4, 8));
 			parent.push_back(make_pair(6, 7));
 			pkb->setChildren(parent);
-
-			vector<int> childrenT = { 5, 6, 7, 8 };
-			pkb->setChildrenT(4, childrenT);
-
-			childrenT.clear();
-			childrenT = { 7 };
-			pkb->setParentT(6, childrenT);
-
-			vector<int> parentT = { 4 };
-			pkb->setParentT(5, parentT);
-			pkb->setParentT(6, parentT);
-			pkb->setParentT(8, parentT);
-
-			parentT.push_back(6);
-			pkb->setParentT(7, parentT);
 
 			// Statement 1 - set constant as variables?
 			pkb->setVarName("beads");
@@ -1101,9 +1040,6 @@ namespace UnitTesting
 			list<string> results = queryEvaluator.evaluateQuery(queryTree);
 			list<string> expectedResult = { "7" };
 
-//			Assert::AreEqual(expectedResult.front(), results.front());
-
-//			Assert::IsTrue(expectedResult.size() == results.size());
 			Assert::IsTrue(expectedResult == results);
 		}
 
@@ -1121,17 +1057,17 @@ namespace UnitTesting
 			Assert::AreEqual(ast3, qe.convertToShuntingYard(equation3));
 		}
 
-		TEST_METHOD(QE_EvaluateSuchThatParentTuple) {
+		TEST_METHOD(QE_EvaluateSuchThatParentTTuple) {
 			/*********************** Test Code ************************/
 			/*	procedure dream {
-			beads = command + 10;							\\1
-			beads = 34;										\\2
-			command = inspiration + 1;						\\3
-			while coffee {									\\4
-			stamps = beads + command + coffee;			\\5
-			while command {								\\6
-			x = x * 9;}								\\7
-			x = beads + command; }}						\\8
+					beads = command + 10;							\\1
+					beads = 34;										\\2
+					command = inspiration + 1;						\\3
+					while coffee {									\\4
+						stamps = beads + command + coffee;			\\5
+						while command {								\\6
+							x = x * 9;}								\\7
+						x = beads + command; }}						\\8
 			*/
 			/**********************************************************/
 			PKB *pkb = new PKB();
@@ -1160,7 +1096,8 @@ namespace UnitTesting
 			follows.push_back(make_pair(2, 3));
 			follows.push_back(make_pair(3, 4));
 			follows.push_back(make_pair(5, 6));
-			follows.push_back(make_pair(7, 8));
+			follows.push_back(make_pair(5, 8));
+			follows.push_back(make_pair(6, 8));
 			pkb->setFollows(follows);
 
 			// Set ALL Parent
@@ -1170,21 +1107,6 @@ namespace UnitTesting
 			parent.push_back(make_pair(4, 8));
 			parent.push_back(make_pair(6, 7));
 			pkb->setChildren(parent);
-
-			vector<int> childrenT = { 5, 6, 7, 8 };
-			pkb->setChildrenT(4, childrenT);
-
-			childrenT.clear();
-			childrenT = { 7 };
-			pkb->setParentT(6, childrenT);
-
-			vector<int> parentT = { 4 };
-			pkb->setParentT(5, parentT);
-			pkb->setParentT(6, parentT);
-			pkb->setParentT(8, parentT);
-
-			parentT.push_back(6);
-			pkb->setParentT(7, parentT);
 
 			// Statement 1 - set constant as variables?
 			pkb->setVarName("beads");
@@ -1266,12 +1188,12 @@ namespace UnitTesting
 
 			pkb->setProcUses(0, varUsed);
 
-			ParserForPQL parserPQL = ParserForPQL("procedure p; stmt s, s1; Select <p, s, s1> such that Parent(s, s1)", *pkb);
+			ParserForPQL parserPQL = ParserForPQL("procedure p; stmt s1; Select <p, s1> such that Parent*(4, s1)", *pkb);
 			QueryTree queryTree = parserPQL.getQueryTree();
 			QueryEvaluator queryEvaluator = QueryEvaluator(*pkb);
 
 			list<string> results = queryEvaluator.evaluateQuery(queryTree);
-			list<string> expectedResults = { "dream, 4, 5", "dream, 4, 6", "dream, 4, 7", "dream, 4, 8", "dream, 6, 5", "dream, 6, 6", "dream, 6, 7", "dream, 6, 8" };
+			list<string> expectedResults = { "dream, 5", "dream, 6", "dream, 7", "dream, 8" };
 
 			Assert::IsTrue(expectedResults == results);
 		}
@@ -1326,21 +1248,6 @@ namespace UnitTesting
 			parent.push_back(make_pair(4, 8));
 			parent.push_back(make_pair(6, 7));
 			pkb->setChildren(parent);
-
-			vector<int> childrenT = { 5, 6, 7, 8 };
-			pkb->setChildrenT(4, childrenT);
-
-			childrenT.clear();
-			childrenT = { 7 };
-			pkb->setParentT(6, childrenT);
-
-			vector<int> parentT = { 4 };
-			pkb->setParentT(5, parentT);
-			pkb->setParentT(6, parentT);
-			pkb->setParentT(8, parentT);
-
-			parentT.push_back(6);
-			pkb->setParentT(7, parentT);
 
 			// Statement 1 - set constant as variables?
 			pkb->setVarName("beads");
@@ -1429,14 +1336,7 @@ namespace UnitTesting
 			list<string> results = queryEvaluator.evaluateQuery(queryTree);
 			list<string> expectedResults = { "2", "3", "4", "6", "8" };
 
-			Assert::AreEqual(expectedResults.size(), results.size());
-	/*		int counter = 0;
-			for (list<string>::iterator it = results.begin(); it != results.end(); it++) {
-				if (counter == 1) {
-					Assert::AreEqual(expectedResults.at(1), *it);
-				}
-				counter++;
-			}*/
+			Assert::IsTrue(expectedResults == results);
 		}
 	};
 };
